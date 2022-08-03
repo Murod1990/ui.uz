8636c671e69ee8262f2b7a93dedc4e0568879b14
Murod <jonsrasulov@gmail.com>
2022-08-03 10:35:36 +0500

    Update SkautingController.php

--- app/Http/Controllers/SkautingController.php
+++ app/Http/Controllers/SkautingController.php
@@ -13,12 +13,7 @@ class SkautingController extends Controller
   //      FROM sekons LEFT JOIN skautings ON sekons.polya_kodi = skautings.skauting_maydon
     //     GROUP BY sekons.ekin_nomi,sekons.aniqlangan_maydon');
      
-    $data = DB::table('sekons')
-->leftJoin('skautings','sekons.polya_kodi','=','skautings.skauting_maydon')
-->selectRaw("sekons.id, COUNT(skautings.skauting_maydon) AS count, SUM(skautings.skauting_foto) as sum")
-->groupBy('sekons.ekin_nomi','sekons.aniqlangan_maydon')
-->orderBy('skautings.skauting_foto')->distinct();
-
+   
         return  $data;
     }
 }
